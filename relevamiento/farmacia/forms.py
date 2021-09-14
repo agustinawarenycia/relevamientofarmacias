86765ee5ad4072deb07a39e67e33ceb30000b2eb
agustinawarenycia <88209286+agustinawarenycia@users.noreply.github.com>
2021-09-14 16:25:22 -0300

    Add files via upload

--- relevamiento/farmacia/forms.py
+++ relevamiento/farmacia/forms.py
@@ -1,8 +1,201 @@
 from django import forms
+from .models import  Fcia, Provincia, Programa, Localidad
+
+
+class LocalidadActForm(forms.ModelForm):
+    class Meta:
+        model = Localidad
+        fields = ['estado']
+        def __init__(self,*args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['estados'].widget.attrs.update({
+                'class': 'form-control'
+            })
+        labels = {
+            'estado' : 'estado de la localidad (Activar/Desactivar)'
+        }
+
+# Formulario para agregar localidad //eliminar probablemente 
+class LocalidadForm(forms.ModelForm):
+    
+    class Meta:
+        # en la subclase Meta del ModelForm se indica el modelo al cual pertenece
+        # el nombre de los campos que deben aparecer
+        # y los widgets que sirven para darle estilos al formulario
+        model = Localidad
+        
+        fields = ['id_localidad','descripcion','id_provincia_id','estado']
+
+        widgets = {
+            'id_localidad': forms.TextInput(attrs={'class': 'form-control'}),
+            'id_provincia_id.descripcion':forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.TextInput(attrs={'class': 'form-control'}),
+            'estado': forms.TextInput(attrs={'class': 'form-control'})
+
+        }
+
+        labels = {
+            'id_localidad': 'id de la localidad',
+            'id_provincia_id.descripcion': 'provincia a la que pertenece',
+            'descripcion': 'nombre de la localidad',
+            'estado':'0=activo y 1 = inactivo'
+        }
+
+#Formulario para agregar/actualizar un programa
+class ProgramaActForm(forms.ModelForm):
+    
+    class Meta:
+
+        model = Programa
+        
+        fields = ['estado']
+
+        def __init__(self,*args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self.fields['estados'].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+        labels = {
+            'estado' : 'estado del programa (Activar/Desactivar)'
+        }
+
+
+
+#Formulario para crear farmacia
+# class FarmaciaForm(forms.ModelForm):
+#     class Meta:     
+#         model = Farmacia
+#         fields = ['fica_id', 'nombre', 'direccion']
+
+#Formualrio para crear provincia
+class ProvinciaForm(forms.ModelForm):
+    class Meta:     
+        model = Provincia
+        fields = ['id_provincia', 'descripcion','estado']
+        widgets={
+            'id_provincia': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.TextInput(attrs={'class': 'form-control'}),
+            'estado': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+        labels = { #permite definir una etiqueta personalizada para cada un de losa tributos
+            'id' : 'id_provincia',
+            'descripcion' : 'nombre de la provincia',
+            'estado': 'estado'
+        }
+
+#Form para actualizar estado de una provincia
+class ProvinciaActForm(forms.ModelForm):
+    class Meta:     
+
+        model = Provincia
+        fields = ['estado']
+        widgets={
+            'estado': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+        labels = { #permite definir una etiqueta personalizada para cada un de losa tributos
+            'estado': 'estado'
+        }
+
+#Formulario para agregar/actualizar un programa
+class ProgramaForm(forms.ModelForm):
+    
+    class Meta:
+        # en la subclase Meta del ModelForm se indica el modelo al cual pertenece
+        # el nombre de los campos que deben aparecer
+        # y los widgets que sirven para darle estilos al formulario
+        model = Programa
+        
+        fields = ['programa','nombre','version','fecha_install']
+
+        widgets = {
+            'programa': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'version': forms.TextInput(attrs={'class': 'form-control'}),
+            'fecha_install': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+        labels = {
+            'programa': 'id del programa',
+            'nombre': 'nombre del programa',
+            'version': 'version del programa actual',
+            'fecha_install': 'fecha de instalacion'
+        }
+
+
+# Formulario para activar provincias
+class ProvinciaActForm(forms.ModelForm):
+    
+    class Meta:
+
+        model = Provincia
+        
+        fields = ['estado']
+
+        def __init__(self,*args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self.fields['estados'].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+        labels = {
+            'estado' : 'estado de la provincia (Activar/Desactivar)'
+        }
+
+
+# Formulario para agregar farmacias
+class FciaForm(forms.ModelForm):
+    
+    class Meta:
+        # en la subclase Meta del ModelForm se indica el modelo al cual pertenece
+        # el nombre de los campos que deben aparecer
+        # y los widgets que sirven para darle estilos al formulario
+        model = Fcia
+        
+        fields = ['nro_cliente','nombre_facia','id_localidad']
+
+        widgets = {
+            'nro_cliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre_facia': forms.TextInput(attrs={'class': 'form-control'}),
+            'id_localidad.descripcion': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+        
+        labels = {
+            
+                'nro_cliente': 'número de cliente',
+                'nombre_facia': 'nombre de la farmacia',
+                'id_localidad.descripcion': 'localidad a la que pertenece'
+            }
+
+#Form para actualizar estado de una fcia
+class FciaActForm(forms.ModelForm):
+    class Meta:
+
+        model = Fcia
+        
+        fields = ['estado']
+
+        def __init__(self,*args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self.fields['estados'].widget.attrs.update({
+                'class': 'form-control'
+            })
+
+        labels = {
+            'estado' : 'estado de la Fcia (Activo/Inatcivo)'
+        }
+from django import forms
 from django.forms.widgets import Widget
 #from django.db import forms
 #from django-autocomplete-light import 
-from .models import  Provincia, Programa, Localidad  #Farmacia,
+from .models import  Fcia, Provincia, Programa, Localidad  #Farmacia,
 
 class LocalidadActForm(forms.ModelForm):
     class Meta:
@@ -157,3 +350,35 @@ class ProvinciaActForm(forms.ModelForm):
             'estado' : 'estado de la provincia (Activar/Desactivar)'
         }
 
+
+
+
+
+
+
+
+
+
+
+
+class FciaForm(forms.ModelForm):
+    
+    class Meta:
+        # en la subclase Meta del ModelForm se indica el modelo al cual pertenece
+        # el nombre de los campos que deben aparecer
+        # y los widgets que sirven para darle estilos al formulario
+        model = Fcia
+        
+        fields = ['nro_cliente','nombre_facia','id_localidad']
+
+        widgets = {
+            'nro_cliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre_facia': forms.TextInput(attrs={'class': 'form-control'}),
+            'id_localidad.descripcion': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+        labels = {
+            'nro_cliente': 'número de cliente',
+            'nombre_facia': 'nombre de la farmacia',
+            'id_localidad.descripcion': 'localidad a la que pertenece'
+        }
--- relevamiento/farmacia/models.py
+++ relevamiento/farmacia/models.py
@@ -1,8 +1,6 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
-from django.db.models.base import Model
-from django.db.models.fields import AutoField, CharField
-from django.forms.models import model_to_dict
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 program_status = [(0,"Desactivar"),(1,"Activar")]
 # Clase que crea una tabla con los datos para los programas instalados
@@ -19,23 +17,17 @@ class Programa(models.Model):
         verbose_name_plural = 'Programas'
 
 
-
-        
 # Tabla Perfil
 class Perfil(models.Model):
-    
     id_perfil = models.CharField("id_perfil", max_length = 256, null = False, blank = False,primary_key=True)
     descripcion = models.CharField("descripción perfil", max_length=256, null = False, blank = False)
-
     def __str__(self):
         return self.descripcion
-
     class Meta:
         verbose_name = 'Perfil'
         verbose_name_plural = 'Perfiles'
 
 # Tabla Usuarios
-
 class Usuario(models.Model):
     id_usuario = models.CharField("id_usuario", max_length=256, null = False, blank = False,primary_key=True)
     nombre = models.CharField("nombre", max_length= 256, null = False, blank = False)
@@ -45,63 +37,44 @@ class Usuario(models.Model):
     id_perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE) # Clave foranea en Django
     def __str__(self):
         return self.nombre
-
     class Meta:
         verbose_name = 'usuario'
         verbose_name_plural = 'usuarios'
 
 # Tabla de Provincias
-
-
 provinica_status = [(0,"Desactivar"),(1,"activar")]
-
 class Provincia(models.Model):
-
     id_provincia = models.IntegerField("id_provincia", null = False, blank = False, primary_key=True)
     descripcion = models.CharField("descripcion", max_length=256, null = False, blank = False)
     estado = models.IntegerField(null=False,blank=False,choices=provinica_status,default=1)
-
     def __str__(self):
         return self.descripcion
-
     class Meta:
         verbose_name = 'provincia'
         verbose_name_plural = 'provincias'
 
 # Tabla localidad 
-
 localidad_status=[(1,"Activar"),(0,"Desactivar")]
-
 class Localidad(models.Model):
-
-    id_localidad = models.IntegerField("id_localidad", null = False, blank = False, primary_key=True)
+    id_localidad = models.IntegerField("Localidad", null = False, blank = False, primary_key=True)
     id_provincia_id = models.ForeignKey(Provincia, on_delete = models.CASCADE)
     descripcion = models.CharField("descripcion", max_length = 256, null = False, blank = False)
     estado = models.IntegerField(null=False, blank=False,choices=localidad_status,default=1)
     def __str__(self):
         return self.descripcion
-
     class Meta:
         verbose_name = 'localidad'
         verbose_name_plural = 'localidades'
 
-        
-
-
-
-# Tabla para Farm
+# Tabla para Farmacias 
+fcia_status =[(1,"Desactivar"),(0,"Activar")]
 class Fcia(models.Model):
-
-    nro_cliente  = models.IntegerField("id_farmacia", default = 1 ,null = False, blank = False, primary_key=True)
-    nombre_facia = models.CharField("nombre_farmacia", max_length = 256, null = False, blank  = False)
-    #direccion = models.CharField("direccion", max_length = 256, null = False, blank = False)
+    nro_cliente  = models.IntegerField("Número de Cliente", default = 1 ,null = False, blank = False, primary_key=True)
+    nombre_facia = models.CharField("nombre_farmacia", max_length = 256, null = True, blank  = True)
     id_localidad = models.ForeignKey(Localidad, on_delete = models.CASCADE)
-    #ip = models.ForeignKey(Pc_Farmacia, on_delete = models.CASCADE)
-    #ip = models.CharField(max_length=256, blank=True,null=True,default="192.168.0.")
-
+    estado = models.IntegerField(null = False, blank = False, choices = fcia_status, default = 0) # en este caso el estado activo es = 0 inactivo = 1
     def __str__(self):
-        return self.nombre_fcia
-
+        return self.nombre_facia
     class Meta:
         verbose_name = 'Fcia'
         verbose_name_plural = 'Fcias'
@@ -109,23 +82,22 @@ class Fcia(models.Model):
 # Tabla para las PC
 class PC_detalles(models.Model):
     id = models.AutoField(primary_key=True)
-    nro_cliente = models.ForeignKey (Fcia,on_delete = models.CASCADE, unique=True, null=True)
     ip = models.CharField("IP",max_length = 255, null = False, blank = False)
     nombre_pc = models.CharField("nombre_pc", max_length = 255, blank = True, null = True)
     id_AnyDesk = models.CharField("any_instalado", max_length = 255, blank = True, null = True)
     fecha_hora = models.DateTimeField("fecha y hora",auto_now_add=True ,null=True,blank=True)
-    
     def __str__(self):
         return str(self.nombre_pc)
     class Meta:
         verbose_name = 'detalle'
         verbose_name_plural = 'detalles'
 
+# Tabla para las Pcs de cada fcia
 class Pc_Farmacia (models.Model):
     ip_publica = models.CharField("ip_publica", primary_key=True, max_length = 256, default="198.162.0.203")
     ip = models.CharField("IP", max_length = 256, null = False, blank = False)
     nro_cliente = models.ForeignKey(Fcia, on_delete=models.CASCADE, null=True)#cambio para filtro de computadoras por farmacia
-    fecha_relevamiento = models.DateField("Fecha-Relevamiento", auto_now_add=True, null = True, blank = True)#agregado    
+    fecha_relevamiento = models.DateField("Fecha-Relevamiento", null = True, blank = True)#agregado    
     nombre_pc = models.CharField("nombre_pc", max_length = 256, blank = True, null = True)
     arquitectura_so = models.CharField("arquitectura_so", max_length = 256, blank = True, null = True)
     version_so = models.CharField("version",max_length=50,blank=True,null=True)#agregado
@@ -147,29 +119,26 @@ class Pc_Farmacia (models.Model):
     procentaje_disponible_D = models.CharField("porcentaje_disponible_D", max_length = 256, null = True, blank = True)#agregado
     AnyDesk_instalado = models.CharField("any_instalado", max_length = 256, blank = True, null = True)
     id_AnyDesk = models.CharField("any_instalado", max_length = 256, blank = True, null = True)
-    
     def __str__(self):
         return str(self.nombre_pc)
-
     class Meta:
         verbose_name = 'computadora'
         verbose_name_plural = 'computadoras'
 
+# Tabla para los programas instalados en cada pc
 class Programas_instalados(models.Model):
     programa_id = models.AutoField(primary_key=True)
-    #fecha_rel = models.DateField("Fecha-Relevamiento", null = True, blank = True)
-    #pc = models.ForeignKey(Pc_Farmacia, on_delete=models.CASCADE, null=True)
-    ip_publica = models.ForeignKey(Pc_Farmacia, on_delete=models.CASCADE, null=True)
+    pc = models.ForeignKey(Pc_Farmacia, on_delete=models.CASCADE, null=True)
     nombre = models.CharField("Nombre del Programa", max_length = 100, null = False, blank = False)
     version = models.CharField("Nombre del Programa", max_length = 100, null = False, blank = True)
     fecha_install = models.CharField("Fecha de isntalación", max_length = 256, null = False, blank = False)
     def __str__(self):
-        return self.nombre
-    
+        return self.nombre    
     class Meta:
         verbose_name = 'Programa_pc'
         verbose_name_plural = 'Programas_pc'
 
+# Tabla para guardar los datos del relevamiento
 class Relevamiento(models.Model):
     id =  models.AutoField(primary_key=True)
     farmacia = models.ForeignKey(Fcia, on_delete=models.CASCADE, null=True)#cambio para filtro de computadoras por farmacia
@@ -197,24 +166,21 @@ class Relevamiento(models.Model):
     procentaje_disponible_D = models.CharField("porcentaje_disponible_D", max_length = 256, null = True, blank = True)#agregado
     AnyDesk_instalado = models.CharField("any_instalado", max_length = 256, blank = True, null = True)
     id_AnyDesk = models.CharField("any_instalado", max_length = 256, blank = True, null = True)
-    
     def __str__(self):
         return str(self.nombre_pc)
-
     class Meta:
         verbose_name = 'relevamiento'
         verbose_name_plural = 'relevamientos'
 
+# Tablas de prueba
 class probando_programas(models.Model):
     id =  models.AutoField(primary_key=True)
     nombre = models.CharField("Nombre del Programa", max_length = 100, null = False, blank = False)
     version = models.CharField("Nombre del Programa", max_length = 100, null = False, blank = True)
     fecha_install = models.CharField("fecha install", max_length = 100, null = False, blank = True)
     pc = models.ForeignKey(Pc_Farmacia,on_delete=models.CASCADE, null=True)
-
     def __str__(self):
         return str(self.nombre)
-
     class Meta:
         verbose_name = 'programa-guardado'
         verbose_name_plural = 'programas-guardados'
@@ -223,6 +189,57 @@ class Cantidad_Programas(models.Model):
     id = models.IntegerField(primary_key=True)
     tabla_programas = models.ForeignKey(Pc_Farmacia,on_delete=models.CASCADE,null=True)
     cantidad = models.IntegerField("cantidad de programas", null=False, blank=False)
-
     class Meta:
-        verbose_name = 'cantidad_programas'
+        verbose_name = 'cantidad_programas' 
+
+
+#-------------------------------MODELO PARA USUARIOS PREDEFINIDOS DE DJANGO---------------------------------
+
+
+# Tabla de Usuarios de Django
+class MainUser(AbstractBaseUser):
+    usuario = models.CharField("Nombre de Usuario", unique = True, max_length = 100)
+    nombre = models.CharField("Nombres", max_length = 256, null = True, blank = True)
+    apellido = models.CharField("Apellidos", max_length = 256, null = True, blank = True)
+    usuario_activo = models.BooleanField(default = True)
+    usuario_admin = models.BooleanField(default = False)
+
+    USERNAME_FIELD = 'usuario'
+    REQUIRED_FIELDS = ['usuario','nombre','apellido']
+    
+    def __str__(self):
+        return f'Usuario: {self.usuario}'
+
+    def has_perm(self,perm,obj=None):
+        return True
+    
+    def has_module_perms(self,app,label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.usuario_admin
+
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self,usuario,nombre,apellido,password=None):
+        if not usuario:
+            raise ValueError('Nombre de usuario obligatorio')
+        usuario = self.model(
+            usuario = usuario,
+            nombre=nombre,
+            apellido=apellido
+        )
+        usuario.set_password(password)
+        usuario.save()
+        return usuario
+    def create_super_user(self,usuario,nombre,apellido,password):
+        usuario = self.create_user(
+            usuario = usuario,
+            nombre=nombre,
+            apellido=apellido,
+            password=password
+        )
+        usuario.usuario_administrador = True
+        usuario.save()
+        return usuario
